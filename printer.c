29e90a0ba26b83defcb4af68889e60dc9724a25b
Gabriel Vasconcelos <gcvasconcelos98@gmail.com>
2019-04-16 17:19:30 -0300

    Added printer file and its header

---
+++ printer.c
@@ -0,0 +1,9 @@
+#include "printer.h"
+
+void classPrinter( classFile* cf) {
+    printf("# Class file Structure\n");
+	printf("CAFEBABE: 0x%0x \n",cf->magic);
+	printf("Minor version: %d \n",cf->minor_version);
+	printf("Major version: %d \n",cf->major_version);
+	printf("Constant Pool Count: %d \n",cf->constant_pool_count);
+}
---
+++ printer.h
@@ -0,0 +1,11 @@
+#ifndef PRINTER_H
+#define PRINTER_H
+
+/* Includes */
+#include <stdlib.h>
+#include <stdio.h>
+#include "reader.h"
+
+/* Functions */
+
+#endif
