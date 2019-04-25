47a949d6ddac7ce6c8c374e1f8cded1a394f4a0c
Gabriel Vasconcelos <gcvasconcelos98@gmail.com>
2019-04-25 19:15:03 -0300

    Added warning and indication comments

--- reader.c
+++ reader.c
@@ -140,6 +140,7 @@ classFile* classReader(char * className) {
       cf->fields[i].attributes[j].attribute_length = read4bytes(file);
       
       uint16_t cp_index = cf->fields[i].attributes[j].attribute_name_index; 
+      /* não estou usando o att length e mallocando o att_info o que pode dar merda */
       if (strcmp((char*)cf->constant_pool[cp_index].info.Utf8.bytes, "ConstantValue")) {
         cf->fields[i].attributes[j].att_info.ConstantValue.constantvalue_index = read1byte(file);
       } else if (strcmp((char*)cf->constant_pool[cp_index].info.Utf8.bytes, "Exceptions")) {
@@ -161,7 +162,9 @@ classFile* classReader(char * className) {
     for(int j = 0; j < cf->methods->attributes_count; j++) {
       cf->methods[i].attributes[j].attribute_name_index = read2bytes(file);
       cf->methods[i].attributes[j].attribute_length = read4bytes(file);
+      
       uint16_t cp_index = cf->methods[i].attributes[j].attribute_name_index; 
+      /* não estou usando o att length e mallocando o att_info o que pode dar merda */
       if (strcmp((char*)cf->constant_pool[cp_index].info.Utf8.bytes, "ConstantValue")) {
         cf->methods[i].attributes[j].att_info.ConstantValue.constantvalue_index = read1byte(file);
       } else if (strcmp((char*)cf->constant_pool[cp_index].info.Utf8.bytes, "Exceptions")) {
@@ -175,10 +178,9 @@ classFile* classReader(char * className) {
   cf->attributes_count = read2bytes(file);
   cf->attributes = (attribute_info *)malloc(cf->attributes_count * sizeof(attribute_info));
   for (int i = 0; i < cf->attributes_count; i++) {
-    
+    /* alguem que ainda não codou tenta implementar pra pegar a logica */
   }
   
-
 	return cf;
 }
 
